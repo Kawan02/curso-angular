7f0464bb68f9d1f1bc7cf64aa26ef090228f5c4f
Kawan02 <kawanng39@gmail.com>
2023-12-30 17:27:49 -0300

    Compartilhamento de dados: componente pai para os componentes filhos usando o pacote Input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { FirstComponentComponent } from "./components/first-component/first-component.component";
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ParentDataComponent } from './components/parent-data/parent-data.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [CommonModule, RouterOutlet, FirstComponentComponent]
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    FirstComponentComponent,
+    ParentDataComponent,
+  ],
 })
 export class AppComponent {
+  userName = 'Joaquim';
+
+  userData = {
+    email: 'teste@gmail.com',
+    role: 'Admin',
+  };
+
   title = 'curso-angular';
 }
