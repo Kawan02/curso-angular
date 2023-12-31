9f64115a06edee013707a8d2c5c2723f514c83f3
Kawan02 <kawanng39@gmail.com>
2023-12-31 18:38:48 -0300

    Exemplo de requisições http com angular

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -11,30 +11,7 @@ import { ListService } from '../../services/list.service';
   styleUrl: './list-render.component.css',
 })
 export class ListRenderComponent {
-  constructor(private listService: ListService) {}
-
-  animals: Animal[] = [
-    {
-      name: 'Turca',
-      type: 'Dog',
-      age: 4,
-    },
-    {
-      name: 'Tom',
-      type: 'Cat',
-      age: 7,
-    },
-    {
-      name: 'Frida',
-      type: 'Dog',
-      age: 9,
-    },
-    {
-      name: 'Bob',
-      type: 'Horse',
-      age: 2,
-    },
-  ];
+  animals: Animal[] = [];
 
   animal: Animal = {
     name: 'Qualquer coisa',
@@ -44,6 +21,9 @@ export class ListRenderComponent {
 
   animalDetails = '';
 
+  constructor(private listService: ListService) {
+    this.getAnimals();
+  }
   showAge(animal: Animal): void {
     this.animalDetails = `O pet ${this.animal.name} tem ${animal.age} anos!`;
   }
@@ -51,5 +31,9 @@ export class ListRenderComponent {
   removeAnimal(animal: Animal) {
     console.log('Removendo animal...');
     this.animals = this.listService.remove(this.animals, animal);
+  }
+
+  getAnimals(): void {
+    this.listService.getAll().subscribe((animals) => (this.animals = animals));
   }
 }
